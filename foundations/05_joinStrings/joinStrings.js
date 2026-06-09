9af03f464ccf0971ebc821cb7ff09beca263bca7
Garrett Howard <garrett3606@gmail.com>
2026-06-09 17:41:40 -0500

    excersises

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,10 +8,14 @@
 */
 
 // Add your code right below, good luck!
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const age = thisYear - birthYear
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + age + " years old."
 
-
-
-
+console.log(greeting)
 
 // Do not change this
 module.exports = {
